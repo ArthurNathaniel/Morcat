3280b1b9fa19f0f98660b2ff32db1cb4db8ac344
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-12-02 07:04:51 -0500

    changes

--- index.php
+++ index.php
@@ -4,49 +4,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="Morcat Entertainment- Your source for buying event tickets online. Find and purchase tickets for concerts, sports events, and more.">
+    <meta name="description" content="Morcat Entertainment- Your source for buying event tickets online. Find and purchase tickets for concerts, sports events, and more.">
     <meta name="keywords" content="E-Ticketing, event tickets, online tickets, concert tickets, sports tickets">
     <meta name="author" content="Arthur Kwabena Larbi Nathaniel">
     <link rel="icon" href="./images/logo.jpg" type="image/png">
     <link rel="apple-touch-icon" sizes="180x180" href="./images/apple.png">
     <title>Morcat Entertainment- Buy Event Tickets Online</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     <link rel="stylesheet" href="./css/index.css">
 </head>
 
 <body>
-  <?php include 'navbar.php'?>
+    <?php include 'navbar.php' ?>
 
     <section>
         <div class="packages nat">
             <div class="package-title">
-          <div class="logo-"></div>
-              <h1>DEMO WEBSITE</h1>
-              <p>
-                For proposal Purpose
-              </p>
-                   
-                    <p class="ticket-number">Ticket Support Numbers: <a href="tel:+233541987478">+233541987478</a></p>
+                <div class="logo-"></div>
+                <h1 class="animate__animated animate__fadeIn animate__delay-1s">DEMO WEBSITE</h1>
+                <p class="animate__animated animate__fadeIn animate__delay-2s">
+                    For proposal Purpose
+                </p>
+                <p class="ticket-number animate__animated animate__fadeIn animate__delay-3s">Ticket Support Numbers:
+                    <a href="tel:+233541987478">+233541987478</a>
+                </p>
             </div>
             <div class="tickettt">
-                <a href="tickets.php" class="ticket-btn">GET YOUR TICKET</a>
+                <a href="tickets.php" class="ticket-btn animate__animated animate__fadeIn animate__delay-4s">GET YOUR
+                    TICKET</a>
             </div>
-            
         </div>
     </section>
 
-  
-
     <section>
         <div class="media">
-            <a href=""><i class="fa-brands fa-x-twitter"></i></a>
-            <a href=""><i class="fa-brands fa-facebook"></i></a>
-            <a href=""><i class="fa-brands fa-youtube"></i></a>
-            <a href=""><i class="fa-brands fa-instagram"></i></a>
+            <a href=""><i class="fa-brands fa-x-twitter animate__animated animate__fadeIn animate__delay-5s"></i></a>
+            <a href=""><i class="fa-brands fa-facebook animate__animated animate__fadeIn animate__delay-6s"></i></a>
+            <a href=""><i class="fa-brands fa-youtube animate__animated animate__fadeIn animate__delay-7s"></i></a>
+            <a href=""><i class="fa-brands fa-instagram animate__animated animate__fadeIn animate__delay-8s"></i></a>
         </div>
     </section>
 
